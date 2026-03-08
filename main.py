df06b91cb2a8ea56596ba37d18f53dcf6502597e
João Costa <PedroM_187@hotmail.com>
2026-03-07 23:52:01 -0300

    feat: adiciona estrutura inicial da classe conta

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 # Arquivo que conterá a interface do programa.
+
+class Conta:
+
+    _contador_ids = 0
+
+    def __init__(self, titular):
+        pass
+    def depositar(self, valor):
+        pass
+    def sacar(self, valor):
+        pass
+    def obter_saldo(self):
+        pass
+    def obter_historico(self):
+        pass
